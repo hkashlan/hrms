8d67f19e19b589de298a1c2c9126de60b16cfc50
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-10-12 09:11:59 +0200

    add material

--- projects/hrms/src/app/app.component.ts
+++ projects/hrms/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatSlideToggleModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
