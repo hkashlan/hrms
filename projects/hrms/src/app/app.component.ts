8aaab04a3e7a80a98720f5c029e0a3ee69f0f914
hadi <hadi@hadis-MacBook-Pro.local>
2025-02-18 06:30:39 +0100

    depends on 1 file

--- projects/hrms/src/app/app.component.ts
+++ projects/hrms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
@@ -6,7 +6,6 @@ import { User } from '@hrms-server/db/schamas/users';
 import { Entity } from 'ui-kit';
 import { userInfo } from './entities/user.entity';
 import { NavigationComponent } from './shell/navigation/navigation.component';
-import { trpc } from './trpc.client';
 
 @Component({
   selector: 'app-root',
@@ -20,76 +19,6 @@ export class AppComponent {
   users = signal<User[]>([]);
   userInfo: Entity<User> = userInfo;
   userControl = new FormControl(null as User | null);
-
-  constructor() {
-    afterNextRender(() => {
-      this.callServer();
-    });
-  }
-
-  async callServer() {
-    // const userInfo: Entity<typeof insertUserSchema.shape> = {
-    //   name: 'User',
-    //   properties: {
-    //     age: {
-    //       type: 'number',
-    //       label: 'Age',
-    //       validation: insertUserSchema.shape.age,
-    //     },
-    //     username: {
-    //       type: 'text',
-    //       label: 'Username',
-    //     },
-    //     email: {
-    //       type: 'text',
-    //       label: 'Email',
-    //     },
-    //     passwordHash: {
-    //       type: 'text',
-    //       label: 'Password',
-    //     },
-    //   },
-    // };
-    // const user = await trpc.login.mutate({ email: 'admin@admin.com', password: '123456' });
-    // await trpc.login.mutate({
-    //   email: 'tt@tt.com',
-    //   password: '123',
-    // });
-    const users = await trpc.user.list.query({
-      // or: [
-      //   {
-      //     and: [
-      //       {
-      //         username: { startsWith: 'John' },
-      //         age: { lte: 40 },
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     username: { startsWith: 'hadi' },
-      //   },
-      // ],
-    });
-    this.users.set(users);
-    // console.log(this.users);
-
-    // await trpc.user.create.mutate({
-    //   email: 'sd',
-    //   username: 'sd',
-    //   passwordHash: 'sd',
-    //   age: 12,
-    // });
-    // await trpc.user.update.mutate({
-    //   id: 1,
-    //   email: 'sd',
-    //   username: 'sd',
-    //   passwordHash: 'sd',
-    //   age: 12,
-    // });
-
-    // await trpc.user.delete.mutate({ id: 1 });
-    // console.log(tt);
-  }
 
   async insertUser() {
     //       label: 'Password',
--- projects/hrms/src/app/pages/detail-page/detail-page.component.ts
+++ projects/hrms/src/app/pages/detail-page/detail-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { User } from '@hrms-server/db/schamas';
+import { DynamicFormComponent, Entity } from 'ui-kit';
+import { userInfo } from '../../entities/user.entity';
 
 @Component({
   selector: 'app-detail-page',
-  imports: [],
-  templateUrl: './detail-page.component.html',
-  styleUrl: './detail-page.component.scss'
+  imports: [DynamicFormComponent, ReactiveFormsModule],
+  template: `
+    <lib-dynamic-form [entity]="userInfo" [formControl]="userControl" />
+  `,
 })
 export class DetailPageComponent {
-
+  userInfo: Entity<User> = userInfo;
+  userControl = new FormControl(null as User | null);
 }
