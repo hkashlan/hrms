75198bf14ee039d8a1b32f7367f3b61163a756a4
hadi <hadi@hadis-MacBook-Pro.local>
2025-03-02 15:42:11 +0100

    remove unneeded routes

--- projects/hrms/src/app/app.routes.ts
+++ projects/hrms/src/app/app.routes.ts
@@ -1,28 +1,18 @@
 import { Routes } from '@angular/router';
 import { DaisyuiComponent } from './pages/daisyui/daisyui.component';
-import { DetailPageComponent } from './pages/detail-page/detail-page.component';
 import { EditEntityInfoComponent } from './pages/edit-entity-info/edit-entity-info.component';
 import { entityRoutes } from './pages/entities/routes';
-import { ListingPageComponent } from './pages/listing-page/listing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'listing/user',
+    redirectTo: 'user/list',
     pathMatch: 'full',
   },
   {
     path: 'login',
     component: LoginComponent,
-  },
-  {
-    path: 'listing/:entity',
-    component: ListingPageComponent,
-  },
-  {
-    path: 'details/:entity/:id',
-    component: DetailPageComponent,
   },
   {
     path: 'daisyui',
