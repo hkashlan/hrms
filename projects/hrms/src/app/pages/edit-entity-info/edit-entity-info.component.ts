587f68d02c46af902b13c6050749f119aee2bc47
hadi <hadi@MacBookPro.fritz.box>
2025-03-04 05:36:14 +0100

    do the up method

--- projects/hrms/src/app/pages/edit-entity-info/edit-entity-info.component.ts
+++ projects/hrms/src/app/pages/edit-entity-info/edit-entity-info.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, input, Signal } from '@angular/core';
+import { Component, computed, input, linkedSignal, Signal } from '@angular/core';
 
 import { BaseProperty } from '@hrms-server/model/property.z';
-import { EmptyObject, entityUtils, KeyProperty } from 'ui-kit';
+import { EmptyObject, Entity, entityUtils, KeyProperty } from 'ui-kit';
 import { EntityKeys } from '../../entities/indext';
 import { EditEntityPropertyComponent } from './edit-entity-property/edit-entity-property.component';
 
@@ -25,10 +25,26 @@ export class EditEntityInfoComponent<T extends EmptyObject = EmptyObject> {
   };
   entity = input.required<EntityKeys>();
 
-  entityInfo = entityUtils.getEntitySignal<T>(this.entity);
+  entityInfo = linkedSignal(entityUtils.getEntitySignal<T>(this.entity));
 
   properties: Signal<KeyProperty<T>[]> = computed(() => {
     const entityInfo = this.entityInfo();
     return entityUtils.getKeyProperties(entityInfo);
   });
+
+  up(index: number) {
+    const properties = this.properties();
+    const [property] = properties.splice(index, 1);
+    properties.splice(index - 1, 0, property);
+    this.updateEntityInfos(properties);
+  }
+
+  private updateEntityInfos(properties: KeyProperty<T>[]) {
+    const entityInfos = this.entityInfo();
+    entityInfos.properties = properties.reduce(
+      (acc, prop) => ({ ...acc, [prop.key]: prop.property }),
+      {} as Entity<T>['properties'],
+    );
+    this.entityInfo.set(entityInfos);
+  }
 }
