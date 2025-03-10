68a3118a20802e1d78d1153cb17b9249c0025273
hadi <hadi@MacBookPro.fritz.box>
2025-03-10 05:51:50 +0100

    add file folder fk

--- projects/hrms-server/src/db/schemas/medias.schema.ts
+++ projects/hrms-server/src/db/schemas/medias.schema.ts
@@ -1,18 +1,31 @@
 import { InferSelectModel } from 'drizzle-orm';
-import { boolean, date, integer, pgEnum, pgTable, text, varchar } from 'drizzle-orm/pg-core';
+import { foreignKey, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { DrizzleTableInfo } from '../../utils/drizzle-table-info';
 import { filterSchema } from '../createFilterSchema';
+import { folders } from './folders.schema';
 
-export const medias = pgTable('medias', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-
-  name: varchar('name'),
-  url: varchar('url'),
-  mimeType: varchar('mimeType'),
-  size: integer('size'),
-});
+export const medias = pgTable(
+  'medias',
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    folderId: integer('folder_id'),
+    name: varchar('name'),
+    url: varchar('url'),
+    mimeType: varchar('mimeType'),
+    size: integer('size'),
+  },
+  (table) => [
+    foreignKey({
+      name: 'media_folder_fk',
+      columns: [table.folderId],
+      foreignColumns: [folders.id],
+    })
+      .onDelete('cascade')
+      .onUpdate('cascade'),
+  ],
+);
 export const insertMediaSchema = createInsertSchema(medias);
 
 export const selectMediaSchema = createSelectSchema(medias);
