9f271dbfe5fd09b6a6fed1246c6ff4a47a9fd9ab
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-03-11 09:30:33 +0300

    use resources

--- projects/hrms/src/app/pages/entities/folder/folder-detail/folder-detail.component.ts
+++ projects/hrms/src/app/pages/entities/folder/folder-detail/folder-detail.component.ts
@@ -1,7 +1,5 @@
-import { Component, input, numberAttribute } from '@angular/core';
-import { Folder, Media } from '@hrms-server/db/schemas';
-import { DetailPageConfig, SafeUrlPipe } from 'ui-kit';
-import { folderInfo } from '../../../../entities/folder.entity';
+import { Component, input, numberAttribute, resource } from '@angular/core';
+import { SafeUrlPipe } from 'ui-kit';
 import { trpc } from '../../../../trpc.client';
 
 @Component({
@@ -11,7 +9,7 @@ import { trpc } from '../../../../trpc.client';
       <section class="mb-8">
         <h2 class="text-2xl font-semibold mb-4">Folders</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          @for (item of folders; track $index) {
+          @for (item of folderResources.value(); track $index) {
             <div class="card w-full bg-base-100 shadow-xl">
               <div class="card-body">
                 <h2 class="card-title">{{ item.name }}</h2>
@@ -28,7 +26,7 @@ import { trpc } from '../../../../trpc.client';
       <section>
         <h2 class="text-2xl font-semibold mb-4">Media</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          @for (item of medias; track $index) {
+          @for (item of mediaResources.value(); track $index) {
             <div class="card w-full bg-base-100 shadow-xl">
               <figure>
                 <iframe [src]="item.url | safeUrl" [attr.type]="item.mimeType">
@@ -51,55 +49,26 @@ import { trpc } from '../../../../trpc.client';
 })
 export class FolderDetailComponent {
   id = input(undefined, { transform: numberAttribute });
-  config: DetailPageConfig<Folder> = {
-    entity: folderInfo,
-    update: trpc.entities.folders.update.mutate,
-    create: trpc.entities.folders.create.mutate,
-    getById: trpc.entities.folders.getById.query,
-  };
 
-  folders: Folder[] = [
-    {
-      id: 1,
-      name: 'Folder 1',
-      parentId: null,
+  folderResources = resource({
+    request: () => ({ id: this.id() }),
+    loader: ({ request }) => {
+      if (!request.id) {
+        return trpc.entities.folders.list.query({});
+      } else {
+        return trpc.entities.folders.list.query({ parentId: { equals: request.id } });
+      }
     },
-    {
-      id: 2,
-      name: 'Folder 2',
-      parentId: null,
-    },
-    {
-      id: 3,
-      name: 'Folder 3',
-      parentId: null,
-    },
-  ];
+  });
 
-  medias: Media[] = [
-    {
-      id: 1,
-      folderId: 1,
-      name: 'Image 1',
-      url: 'https://placehold.co/400x225',
-      mimeType: 'image/jpeg',
-      size: 123456,
+  mediaResources = resource({
+    request: () => ({ id: this.id() }),
+    loader: ({ request }) => {
+      if (!request.id) {
+        return trpc.entities.medias.list.query({});
+      } else {
+        return trpc.entities.medias.list.query({ folderId: { equals: request.id } });
+      }
     },
-    {
-      id: 2,
-      folderId: 1,
-      name: 'Video 1',
-      url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-      mimeType: 'video/mp4',
-      size: 123456,
-    },
-    {
-      id: 3,
-      folderId: 1,
-      name: 'pdf 2',
-      url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
-      mimeType: 'image/jpeg',
-      size: 123456,
-    },
-  ];
+  });
 }
