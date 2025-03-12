3ff9f01cbd23a9dcaff67c086f66b7246d18d614
hadi <hadi@hadis-MacBook-Pro.local>
2025-03-12 07:52:12 +0100

    show warning for delete

--- projects/hrms/src/app/pages/entities/folder/folder-detail/sub-folder-list/sub-folder-list.component.ts
+++ projects/hrms/src/app/pages/entities/folder/folder-detail/sub-folder-list/sub-folder-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, input, resource } from "@angular/core";
-import { ButtonDirective } from "daisyui";
-import { FolderDialogComponent } from "../add-folder/folder-dialog.component";
-import { trpc } from "./../../../../../trpc.client";
+import { Component, input, resource } from '@angular/core';
+import { ButtonDirective } from 'daisyui';
+import { FolderDialogComponent } from '../add-folder/folder-dialog.component';
+import { trpc } from './../../../../../trpc.client';
 
 @Component({
-  selector: "app-sub-folder-list",
+  selector: 'app-sub-folder-list',
   imports: [FolderDialogComponent, ButtonDirective],
   template: `
     <section class="mb-8">
@@ -12,21 +12,31 @@ import { trpc } from "./../../../../../trpc.client";
         Folders
         <button class="btn" (click)="folderDlg.showModal()">add modal</button>
       </h2>
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-      >
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         @for (item of folderResources.value(); track $index) {
-        <div class="card w-full bg-base-100 shadow-xl">
-          <div class="card-body">
-            <h2 class="card-title">{{ item.name }}</h2>
-            <p>Contains files and subfolders.</p>
-            <div class="card-actions justify-end">
-              <button [duiButton]="'btn-primary'">Open</button>
-              <!-- <button [duiButton]="'btn-error'" (click)="deleteFolder(item.id, item.name)">delete</button> -->
-              <button (click)="deleteFolder(item.id, item.name)">delete</button>
+          <div class="card w-full bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">{{ item.name }}</h2>
+              <p>Contains files and subfolders.</p>
+              <div class="card-actions justify-end">
+                <button [duiButton]="'btn-primary'">Open</button>
+                <!-- <button [duiButton]="'btn-error'" (click)="deleteFolder(item.id, item.name)">delete</button> -->
+                <button [duiButton]="'btn-error'" (click)="confirmDialog.showModal()">
+                  delete
+                </button>
+                <dialog #confirmDialog class="modal">
+                  <div class="modal-box">
+                    <h3 class="font-bold text-lg">{{ item.name }}</h3>
+                    <p class="py-4">{{ item.name }}</p>
+                    <div class="modal-action">
+                      <button class="btn btn-primary" (click)="deleteFolder(item.id)">Yes</button>
+                      <button class="btn btn-error" (click)="confirmDialog.close()">No</button>
+                    </div>
+                  </div>
+                </dialog>
+              </div>
             </div>
           </div>
-        </div>
         }
       </div>
     </section>
@@ -60,13 +70,9 @@ export class SubFolderListComponent {
       this.folderResources.reload();
     }
   }
-  deleteFolder(id: number, name: string | null) {
-    const folderName = name ?? "Unnamed Folder";
-
-    // if (confirm(`Are you sure you want to delete "${folderName}"?`)) {
+  deleteFolder(id: number) {
     trpc.entities.folders.delete.mutate({ id }).then(() => {
       this.folderResources.reload();
     });
-    // }
   }
 }
